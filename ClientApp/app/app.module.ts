e947287284739e375fd943774dbec82270a469e6
bradonf333 <bradonf123@gmail.com>
2017-10-01 19:03:54 -0600

    Generated vehicle-form component

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppModuleShared } from './app.module.shared';
 import { AppComponent } from './components/app/app.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [
         ServerModule,
         AppModuleShared
-    ]
+    ],
+    declarations: [VehicleFormComponent]
 })
 export class AppModule {
 }
