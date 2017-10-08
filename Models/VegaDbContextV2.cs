9e1907d032d10de7ea87b969db6037df09b42606
bradonf333 <bradonf123@gmail.com>
2017-10-07 21:29:53 -0600

    Created VehicleFeature and db migration/update

--- Models/VegaDbContextV2.cs
+++ Models/VegaDbContextV2.cs
@@ -4,12 +4,20 @@ namespace vega.Models
 {
     public class VegaDbContextV2 : DbContext
     {
-        public VegaDbContextV2(DbContextOptions<VegaDbContextV2> options) : base(options)
-        {
-            
-        }
-
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public VegaDbContextV2(DbContextOptions<VegaDbContextV2> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf => new
+            {
+                vf.VehicleId,
+                vf.FeatureId
+            });
+        }
     }
 }
