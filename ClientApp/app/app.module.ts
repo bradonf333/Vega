918f35c49e26ecaf033127f3f870c65c2af75462
bradonf333 <bradonf123@gmail.com>
2018-01-28 19:50:09 -0700

    Pagination component

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppModuleShared } from './app.module.shared';
 import { AppComponent } from './components/app/app.component';
+import { PaginationComponent } from './shared/pagination.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [
         ServerModule,
         AppModuleShared
-    ]
+    ],
+    declarations: [PaginationComponent]
 })
 export class AppModule {
 }
