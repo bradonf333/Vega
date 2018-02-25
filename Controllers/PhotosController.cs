f1690bd3f015e68b9c7e7b0259b6794e48722ba5
bradonf333 <bradonf123@gmail.com>
2018-02-24 22:23:20 -0700

    Validation for Photos

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
@@ -9,14 +10,25 @@ using vega.Controllers.Resources;
 using vega.Core.Models;
 using vega.Models;
 
-namespace vega.Controllers {
+namespace vega.Controllers
+{
     [Route("/api/vehicles/{vehicleId}/photos")]
-    public class PhotosController : Controller {
+    public class PhotosController : Controller
+    {
         private readonly IHostingEnvironment host;
         private readonly IVehicleRepository repository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
-        public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper) 
+        private readonly int MAX_BYTES = 1 * 1024 * 1024;
+        private readonly string[] ACCEPTED_FILE_TYPES = new []
+        {
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".gif"
+        };
+
+        public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
         {
             this.mapper = mapper;
             this.unitOfWork = unitOfWork;
@@ -25,16 +37,29 @@ namespace vega.Controllers {
         }
 
         [HttpPost]
-        public async Task<IActionResult> Upload(int vehicleId, IFormFile file) 
+        public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
         {
-            var vehicle = await repository.GetVehicleAsync(vehicleId, includeRelated: false);
-            if(vehicle == null)
+            var vehicle = await repository.GetVehicleAsync(vehicleId, includeRelated : false);
+            if (vehicle == null)
             {
                 return NotFound();
             }
 
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File is null or Empty.");
+            }
+            if (file.Length > MAX_BYTES)
+            {
+                return BadRequest("Maximum file size exceeded.");
+            }
+            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName)))
+            {
+                return BadRequest("Invalid file type.");
+            }
+
             var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
-            if(!Directory.Exists(uploadsFolderPath)) 
+            if (!Directory.Exists(uploadsFolderPath))
             {
                 Directory.CreateDirectory(uploadsFolderPath);
             }
@@ -42,12 +67,12 @@ namespace vega.Controllers {
             var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
             var filePath = Path.Combine(uploadsFolderPath, fileName);
 
-            using(var stream = new FileStream(filePath, FileMode.Create)) 
+            using(var stream = new FileStream(filePath, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
 
-            var photo = new Photo 
+            var photo = new Photo
             {
                 FileName = fileName
             };
